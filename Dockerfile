ac8c714cceee148aaaab5e5f8a983aafcb66247c
PhoenixGitH <el.mentor.n1@gmail.com>
2024-09-04 02:22:47 +0200

    Remove not need objects + docker
    - Implementing example api to check docker compatibility
    
    - Remove not necesary objects from project.
    
    - Move to a lighter IDE like Visual Studio Code.

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-nanoserver-1809 AS base
+WORKDIR /app
+EXPOSE 5000
+ENV ASPNETCORE_URLS=http://*:5000
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0-nanoserver-1809 AS build
+ARG configuration=Release
+WORKDIR /src
+COPY ["BankAPI.csproj", "./"]
+RUN dotnet restore "BankAPI.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "BankAPI.csproj" -c $configuration -o /app/build
+
+FROM build AS publish
+ARG configuration=Release
+RUN dotnet publish "BankAPI.csproj" -c $configuration -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "BankAPI.dll"]
---
+++ Models/BankModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankAPI.Models
+{
+    public class BankModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BankID { get; set; }
+        public string Bic { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+
+        public BankModel(string bic, string country, string name, int id)
+        {
+            this.BankID = id;
+            this.Bic = bic;
+            this.Country = country;
+            this.Name = name;
+        }
+
+    }
+}
